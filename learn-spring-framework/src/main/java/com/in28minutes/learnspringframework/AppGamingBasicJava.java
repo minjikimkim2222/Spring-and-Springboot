b2de9b475cb280dab2da1e24a5329dc083304310
김민지 <olivia0929@naver.com>
2023-11-08 15:51:41 +0900

    tightly coupled java code

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -0,0 +1,18 @@
+package com.in28minutes.learnspringframework;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.SuperContraGame;
+
+public class AppGamingBasicJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		var marioGame = new MarioGame();
+		
+		var superContraGame = new SuperContraGame();
+		var gameRunner = new GameRunner(superContraGame);
+		gameRunner.run();
+	}
+
+}
