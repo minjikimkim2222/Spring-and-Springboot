1ad72ec2072cf4a4717f47feeef293d4e146865a
김민지 <olivia0929@naver.com>
2023-11-08 19:24:24 +0900

    첫번째 Java Spring Bean

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,12 +3,41 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age) {
+};
+
+record Address(String firstLine, String city) {
+
+};
+
 @Configuration
 public class HelloWorldConfiguration {
 
 	@Bean
 	public String name() {
 		return "Minji";
+	}
+
+	@Bean
+	public int age() {
+		return 15;
+	}
+
+	@Bean
+	public Person person() {
+		var person = new Person("hj", 20); // Person record 생성자
+//
+//		person.age(); // getter 메서드 대신
+//		person.name(); // getter 메서드가 자동으로
+		return (person);
+
+	}
+
+	@Bean
+	public Address address() {
+		var address = new Address("firstline", "Seoul");
+
+		return (address);
 	}
 
 }
