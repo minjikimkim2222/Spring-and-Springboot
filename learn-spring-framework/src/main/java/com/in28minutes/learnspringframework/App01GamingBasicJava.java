57e2c64c1ec315bb288d0774e6229ae8232e5fb7
김민지 <olivia0929@naver.com>
2023-11-08 18:56:59 +0900

    첫번째 Java Spring Bean

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
@@ -5,15 +5,17 @@ import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.PacmanGame;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //	var game = new MarioGame();
 		
 //	var game = new SuperContraGame();
-		var game = new PacmanGame();
+		var game = new PacmanGame(); // 1. object creation
 		var gameRunner = new GameRunner(game);
+		// 2 : Object Creation + Wiring of Dependency (의존성을 연결)
+		// game is a Dependency of GameRunner 
 		gameRunner.run();
 	}
 
