092167e1caeaed45fc2a5c1ee7e3b14f4c4f4860
minjikimkim2222 <olivia0929@naver.com>
2023-12-29 19:40:43 +0900

    Spring Data Jpa start

--- springboot-with-jpa-and-hibernate/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ springboot-with-jpa-and-hibernate/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -4,26 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
+
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
 
 //	@Autowired
 //	private CourseJdbcRepository repository;
 
+//	@Autowired
+//	private CourseJpaRepository repository;
+
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringDataJpaRepository repository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		repository.insert(new Course(2, "db", "minji2"));
-		repository.insert(new Course(3, "java", "minji3"));
-		repository.insert(new Course(4, "spring", "minji4"));
+		repository.save(new Course(2, "db", "minji2"));
+		repository.save(new Course(3, "java", "minji3"));
+		repository.save(new Course(4, "spring", "minji4"));
 
-		repository.deleteById(2);
+		repository.deleteById(2l);
 
-		System.out.println(repository.findById(4));
-		System.out.println(repository.findById(3));
+		System.out.println(repository.findById(4l));
+		System.out.println(repository.findById(3l));
 
 	}
 
