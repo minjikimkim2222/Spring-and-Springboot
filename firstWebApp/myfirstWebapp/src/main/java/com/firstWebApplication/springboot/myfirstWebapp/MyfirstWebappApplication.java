57e460627619006b015f31eefc4a2c0c3596b929
minjikimkim2222 <olivia0929@naver.com>
2024-01-10 22:25:53 +0900

    update all changes

---
+++ firstWebApp/myfirstWebapp/src/main/java/com/firstWebApplication/springboot/myfirstWebapp/MyfirstWebappApplication.java
@@ -0,0 +1,13 @@
+package com.firstWebApplication.springboot.myfirstWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstWebappApplication.class, args);
+	}
+
+}
---
+++ firstWebApp/myfirstWebapp/src/test/java/com/firstWebApplication/springboot/myfirstWebapp/MyfirstWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstWebApplication.springboot.myfirstWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
