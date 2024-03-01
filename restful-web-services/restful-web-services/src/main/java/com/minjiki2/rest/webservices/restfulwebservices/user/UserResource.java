691248539fb6cf9d856171cd66b5fd90a6db2536
minjikimkim2222 <olivia0929@naver.com>
2024-03-01 17:54:37 +0900

    restapi - delete method add

--- restful-web-services/restful-web-services/src/main/java/com/minjiki2/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/restful-web-services/src/main/java/com/minjiki2/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,5 +52,11 @@ public class UserResource {
 				.path("/{id}").buildAndExpand(savedUser.getId()).toUri();
 
 		return ResponseEntity.created(location).build();
+	}
+
+	// DELETE /users/{id}
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable("id") int id) {
+		service.deleteById(id);
 	}
 }
