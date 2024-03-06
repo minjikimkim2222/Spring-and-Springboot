93619db89e9eaf59c43837ac312affe2124a1e99
minjikimkim2222 <olivia0929@naver.com>
2024-03-06 22:47:28 +0900

    rest api 국제화, rest api versioning with URL

--- restful-web-services/restful-web-services/src/main/java/com/minjiki2/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/restful-web-services/src/main/java/com/minjiki2/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,24 @@
 package com.minjiki2.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+	private MessageSource messageSource;
+
+	@Autowired
+	public HelloWorldController(MessageSource messageSource) {
+		super();
+		this.messageSource = messageSource;
+	}
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -25,7 +38,9 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world-internationalized")
 	public String helloWorldInternationalized() {
-		return "hello world v2";
+		Locale locale = LocaleContextHolder.getLocale();
+
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 //		- Example: `en` - English (Good Morning)
 //		- Example: `nl` - Dutch (Goedemorgen)
 //		- Example: `fr` - French (Bonjour)
