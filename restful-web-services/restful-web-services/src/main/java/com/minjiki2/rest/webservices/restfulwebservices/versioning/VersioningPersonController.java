c2e26c44430197696df29223e9ce2a95f2a71894
minjikimkim2222 <olivia0929@naver.com>
2024-03-06 23:25:51 +0900

    restapi versioning - request parameter, headers, media type versioning

--- restful-web-services/restful-web-services/src/main/java/com/minjiki2/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ restful-web-services/restful-web-services/src/main/java/com/minjiki2/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -15,4 +15,35 @@ public class VersioningPersonController {
 	public PersonV2 getSecondVersionOfPerson() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+
+	// http://localhost:8080/person?version=1
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 getFirstVersionOfPersonRequestParameter() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(path = "/person", params = "version=2")
+	public PersonV2 getSecondVersionOfPersonRequestParameter() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 getFirstVersionOfPersonRequestHeader() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 getSecondVersionOfPersonRequestHeader() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+
+	@GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
 }
